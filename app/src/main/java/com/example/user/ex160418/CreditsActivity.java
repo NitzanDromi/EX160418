03d0300bd517125dd8aefe98fa3348b71fed8d64
NitzanDromi <nd0389@bs.amalnet.k12.il>
2018-05-07 00:41:41 +0300

    1st

--- app/src/main/java/com/example/user/ex160418/CreditsActivity.java
+++ app/src/main/java/com/example/user/ex160418/CreditsActivity.java
@@ -11,6 +11,7 @@ public class CreditsActivity extends AppCompatActivity {
     TextView answerr;
     Button ret;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
